aebc26dc8180b5c94e2d81e314d8292a831c9eff
Misnag <n8zdahh6k93wf3e4ambz@docomo.ne.jp>
2022-08-03 04:38:32 +0900

    [add]記事詳細画面

---
+++ blog/app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Post;
+use App\Http\Request\PostRequest;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post ->getPaginateByLimit()]);
+    } 
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+}
+?>
