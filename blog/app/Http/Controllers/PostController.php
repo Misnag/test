28a66b2c203078ce72cba2258688fb6ad71b9143
Misnag <n8zdahh6k93wf3e4ambz@docomo.ne.jp>
2022-08-06 07:40:14 +0900

    08おわり

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -2,20 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use App\Category;
 use App\Post;
-use App\Http\Request\PostRequest;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
+
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post ->getPaginateByLimit()]);
+        return view('posts/index')->with(['posts' => $post ->getPaginate()]);
     } 
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('posts/create'); 
+    }
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+    return view('posts/edit')->with(['post' => $post]);
+    }
+    public function update(Request $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        
+        return redirect('/posts/' . $post->id);
+    }
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
 ?>
--- blog/app/Post.php
+++ blog/app/Post.php
@@ -3,10 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    public function getPaginateByLimit(int $limit_count = 5)
+    use SoftDeletes;
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
+    public function getPaginate(int $limit_count = 10)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
